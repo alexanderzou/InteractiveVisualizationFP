727ddf5e24546cd072824c4b869d561b78d8642d
Alexander Zou <alexanderzou2@gmail.com>
2024-04-10 22:41:09 -0400

    touched stuff up

--- scripts/process_vax.py
+++ scripts/process_vax.py
@@ -1,6 +1,10 @@
 import pprint
 
-#https://eds.p.ebscohost.com/eds/pdfviewer/pdfviewer?vid=4&sid=de3db832-4381-4401-aa27-9ac11c4aebc5%40redis
+#Color aesthetics with regard to product design and multimedia web pages
+#https://eds.p.ebscohost.com/eds/pdfviewer/pdfviewer?vid=1&sid=343c0a69-29b8-40d8-a145-c9718a483064%40redis
+
+#Investigating Color-Blind User-Interface Accessibility via Simulated Interfaces
+#https://mdpi-res.com/computers/computers-13-00053/article_deploy/computers-13-00053.pdf?version=1708147771
 
 f = open("../datafiles/Archive__COVID-19_Vaccination_and_Case_Trends_by_Age_Group__United_States.csv")
 lines = f.readlines()
